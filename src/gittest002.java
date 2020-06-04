c554700274dfba2f91814e3bbd9d2010fd43f4ea
yoYWH <hedgeh0g@yeah.net>
2020-06-05 00:56:33 +0800

    afafqwfqwfqwfqwfqwfwqf

---
+++ src/gittest002.java
@@ -0,0 +1,8 @@
+public class gittest002 {
+    public static void main(String[] args) {
+        System.out.println("jiajuesssb");
+        System.out.println("jiajuesssb");
+        System.out.println("jiajuesssb");
+
+    }
+}
